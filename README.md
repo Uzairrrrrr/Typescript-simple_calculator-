f6907ed68a229cf4b93ab1ba53b5ea4a1e2d111b
uzairrrrrr <uzairawan156@gmail.com>
2024-03-20 15:06:38 +0500

    simple calculator

---
+++ README.md
@@ -0,0 +1,1 @@
+### Simple Calculator using Typescript and Inquirer
---
+++ index.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstnumber" },
+  { message: "Enter Second Number", type: "number", name: "secondnumber" },
+  {
+    message: "Select One Operator",
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
+]);
+if (answer.operators === "Addition") {
+  console.log(
+    "The Addition of these two numbers are",
+    answer.firstnumber + answer.secondnumber
+  );
+} else if (answer.operators === "Substraction") {
+  console.log(
+    "The Substraction of these two numbers are",
+    answer.firstnumber - answer.secondnumber
+  );
+} else if (answer.operators === "Multiplication"){
+  console.log(
+    "The Multiplication of these two numbers are",
+    answer.firstnumber * answer.secondnumber
+  );
+}
+else if (answer.operators === "Division"){
+  console.log(
+    "The Division of these two numbers are",
+    answer.firstnumber / answer.secondnumber
+  );
+}
